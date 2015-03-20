805d3f5f916e9b2b6ef02cbc48cdbeb9976c4011
Will <whp3652@rit.edu>
2015-03-20 10:44:11 -0400

    Get the mpr and 9dof working together in a chain...sort of

--- mouse_rat.ino
+++ mouse_rat.ino
@@ -2,6 +2,7 @@
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_LSM9DS0.h>
+#include "Adafruit_MPR121.h"
 
 /* This driver uses the Adafruit unified sensor library (Adafruit_Sensor),
    which provides a common 'type' for sensor data and some helper functions.
@@ -28,7 +29,9 @@
 */
 
 /* Assign a unique base ID for this sensor */
-Adafruit_LSM9DS0 lsm = Adafruit_LSM9DS0(1000);  // Use I2C, ID #1000
+
+// 9DOF
+Adafruit_LSM9DS0 lsm = Adafruit_LSM9DS0(1005);  // Use I2C, ID #1000
 #define LSM9DS0_XM_CS 10
 #define LSM9DS0_GYRO_CS 9
 #define LSM9DS0_SCLK 13
@@ -37,9 +40,158 @@ Adafruit_LSM9DS0 lsm = Adafruit_LSM9DS0(1000);  // Use I2C, ID #1000
 
 float xNormal = -3.0;
 float yNormal = 1;
-
 float xRange[] = {0,0};
 float yRange[] = {0,0};
+
+// MPR121
+// You can have up to 4 on one i2c bus but one is enough for testing!
+Adafruit_MPR121 cap = Adafruit_MPR121();
+
+uint16_t lasttouched = 0;
+uint16_t currtouched = 0;
+
+// Configures the gain and integration time for the TSL2561
+
+void configureSensor(void) {
+  // 1.) Set the accelerometer range
+  //lsm.setupAccel(lsm.LSM9DS0_ACCELRANGE_2G);
+  lsm.setupAccel(lsm.LSM9DS0_ACCELRANGE_4G);
+  //lsm.setupAccel(lsm.LSM9DS0_ACCELRANGE_6G);
+  //lsm.setupAccel(lsm.LSM9DS0_ACCELRANGE_8G);
+  //lsm.setupAccel(lsm.LSM9DS0_ACCELRANGE_16G);
+
+  // 2.) Set the magnetometer sensitivity
+  lsm.setupMag(lsm.LSM9DS0_MAGGAIN_2GAUSS);
+  //lsm.setupMag(lsm.LSM9DS0_MAGGAIN_4GAUSS);
+  //lsm.setupMag(lsm.LSM9DS0_MAGGAIN_8GAUSS);
+  //lsm.setupMag(lsm.LSM9DS0_MAGGAIN_12GAUSS);
+
+  // 3.) Setup the gyroscope
+  lsm.setupGyro(lsm.LSM9DS0_GYROSCALE_245DPS);
+  //lsm.setupGyro(lsm.LSM9DS0_GYROSCALE_500DPS);
+  //lsm.setupGyro(lsm.LSM9DS0_GYROSCALE_2000DPS);
+}
+
+void setup(void) {
+  Serial.begin(9600);
+  
+  dof_setup();
+  mpr_setup();
+}
+
+
+void mpr_setup() {
+  while (!Serial);        // needed to keep leonardo/micro from starting too fast!
+
+  Serial.begin(9600);
+  Serial.println("Adafruit MPR121 Capacitive Touch sensor test"); 
+  
+  // Default address is 0x5A, if tied to 3.3V its 0x5B
+  // If tied to SDA its 0x5C and if SCL then 0x5D
+  if (!cap.begin(0x5A)) {
+    Serial.println("MPR121 not found, check wiring?");
+    while (1);
+  }
+  Serial.println("MPR121 found!");
+}
+
+void dof_setup() {
+  // 9DOF
+  if(!lsm.begin()) { while(1); }
+
+  /* Setup the sensor gain and integration time */
+  configureSensor();
+  
+  // Set a max and min value to not do anything
+  // calibrate(accel.acceleration.x, accel.acceleration.y);
+  
+  sensors_event_t accel, mag, gyro, temp;
+  lsm.getEvent(&accel, &mag, &gyro, &temp);  
+}
+
+void loop(void) {
+  dof_loop();
+  mpr_loop();
+}
+
+void dof_loop() {
+  /* Get a new sensor event */
+  sensors_event_t accel, mag, gyro, temp;
+
+  lsm.getEvent(&accel, &mag, &gyro, &temp);
+
+  float mx, my = 0.0;
+//  float accel_x = clamp(accel.acceleration.y, -20, 20)
+
+  if (accel.acceleration.x > xRange[1] || accel.acceleration.x < xRange[0]) {
+    my = map(accel.acceleration.x - xNormal, -20, 20, 200, -200);
+    mx = map(accel.acceleration.y - yNormal, -20, 20, 200, -200);
+  }
+  else if (accel.acceleration.y > yRange[1] || accel.acceleration.y < yRange[0]) {
+    my = map(accel.acceleration.x - xNormal, -20, 20, 200, -200);
+    mx = map(accel.acceleration.y - yNormal, -20, 20, 200, -200);
+  }
+  Mouse.move(mx, my, 0);
+  delay(10);
+}
+
+void mpr_loop() {
+ // Get the currently touched pads
+  currtouched = cap.touched();
+  
+  for (uint8_t i=0; i < 12; i++) {
+    // it if *is* touched and *wasnt* touched before, alert!
+    if ((currtouched & _BV(i)) && !(lasttouched & _BV(i)) ) {
+      Serial.print(i); Serial.println(" touched");
+    }
+    // if it *was* touched and now *isnt*, alert!
+    if (!(currtouched & _BV(i)) && (lasttouched & _BV(i)) ) {
+      Serial.print(i); Serial.println(" released");
+    }
+  }
+
+  // reset our state
+  lasttouched = currtouched;
+
+  // comment out this line for detailed data from the sensor!
+  return;
+  
+  // debugging info, what
+  Serial.print("\t\t\t\t\t\t\t\t\t\t\t\t\t 0x"); Serial.println(cap.touched(), HEX);
+  Serial.print("Filt: ");
+  for (uint8_t i=0; i < 12; i++) {
+    Serial.print(cap.filteredData(i)); Serial.print("\t");
+  }
+  Serial.println();
+  Serial.print("Base: ");
+  for (uint8_t i=0; i < 12; i++) {
+    Serial.print(cap.baselineData(i)); Serial.print("\t");
+  }
+  Serial.println();
+  
+  // put a delay so it isn't overwhelming
+  delay(100); 
+}
+
+void calibrate(float x, float y) {
+   xRange[0] = min(xRange[0], x);
+   xRange[1] = max(xRange[1], x);
+   
+   yRange[0] = min(yRange[0], y);
+   yRange[1] = max(yRange[1], y);
+   
+   xNormal = (xRange[0] + xRange[1])/2;
+   yNormal = (yRange[0] + yRange[1])/2;
+   
+   Serial.print(xRange[0]); Serial.print(", "); Serial.println(xRange[1]);
+   Serial.print(yRange[0]); Serial.print(", "); Serial.println(yRange[1]);
+}
+
+//template<typename T>
+//T clamp(T Value, T Min, T Max) {
+//  return (Value < Min)? Min : (Value > Max)? Max : Value;
+//}
+
 
 void displaySensorDetails(void) {
   sensor_t accel, mag, gyro, temp;
@@ -87,102 +239,4 @@ void displaySensorDetails(void) {
   Serial.println(F(""));
 
   delay(500);
-}
-
-/**************************************************************************/
-/*
-    Configures the gain and integration time for the TSL2561
-*/
-/**************************************************************************/
-void configureSensor(void) {
-  // 1.) Set the accelerometer range
-  //lsm.setupAccel(lsm.LSM9DS0_ACCELRANGE_2G);
-  lsm.setupAccel(lsm.LSM9DS0_ACCELRANGE_4G);
-  //lsm.setupAccel(lsm.LSM9DS0_ACCELRANGE_6G);
-  //lsm.setupAccel(lsm.LSM9DS0_ACCELRANGE_8G);
-  //lsm.setupAccel(lsm.LSM9DS0_ACCELRANGE_16G);
-
-  // 2.) Set the magnetometer sensitivity
-  lsm.setupMag(lsm.LSM9DS0_MAGGAIN_2GAUSS);
-  //lsm.setupMag(lsm.LSM9DS0_MAGGAIN_4GAUSS);
-  //lsm.setupMag(lsm.LSM9DS0_MAGGAIN_8GAUSS);
-  //lsm.setupMag(lsm.LSM9DS0_MAGGAIN_12GAUSS);
-
-  // 3.) Setup the gyroscope
-  lsm.setupGyro(lsm.LSM9DS0_GYROSCALE_245DPS);
-  //lsm.setupGyro(lsm.LSM9DS0_GYROSCALE_500DPS);
-  //lsm.setupGyro(lsm.LSM9DS0_GYROSCALE_2000DPS);
-}
-
-/**************************************************************************/
-/*
-    Arduino setup function (automatically called at startup)
-*/
-/**************************************************************************/
-void setup(void) {
-  Serial.begin(9600);
-
-  /* Initialise the sensor */
-  if(!lsm.begin()) { while(1); }
-
-  /* Setup the sensor gain and integration time */
-  configureSensor();
-  
-  sensors_event_t accel, mag, gyro, temp;
-  lsm.getEvent(&accel, &mag, &gyro, &temp);  
-  
-  calibrate(accel.acceleration.x, accel.acceleration.y);
-}
-
-void loop(void) {
-  /* Get a new sensor event */
-  sensors_event_t accel, mag, gyro, temp;
-
-  lsm.getEvent(&accel, &mag, &gyro, &temp);
-
-  float mx, my = 0.0;
-  float accel_x = clamp(accel.acceleration.y, -20, 20)
-
-  if (accel.acceleration.x > xRange[1] || accel.acceleration.x < xRange[0]) {
-    my = map(accel.acceleration.x - xNormal, -20, 20, 200, -200);
-    mx = map(accel.acceleration.y - yNormal, -20, 20, 200, -200);
-  }
-  else if (accel.acceleration.y > yRange[1] || accel.acceleration.y < yRange[0]) {
-    my = map(accel.acceleration.x - xNormal, -20, 20, 200, -200);
-    mx = map(accel.acceleration.y - yNormal, -20, 20, 200, -200);
-  }
-  Mouse.move(mx, my, 0);
-
-//  print out accelleration data
-//  Serial.print("Accel X: "); Serial.print(accel.acceleration.x); Serial.print(" ");
-//  Serial.print("  \tY: "); Serial.print(accel.acceleration.y);   Serial.print(" ");
-//  Serial.print("  \tZ: "); Serial.print(accel.acceleration.z);   Serial.println("  \tm/s^2");
-//  Serial.print("Magn. X: "); Serial.print(mag.magnetic.x); Serial.print(" ");
-//  Serial.print("  \tY: "); Serial.print(mag.magnetic.y);       Serial.print(" ");
-//  Serial.print("  \tZ: "); Serial.print(mag.magnetic.z);     Serial.println("  \tgauss");
-//  Serial.print("Gyro  X: "); Serial.print(gyro.gyro.x); Serial.print(" ");
-//  Serial.print("  \tY: "); Serial.print(gyro.gyro.y);       Serial.print(" ");
-//  Serial.print("  \tZ: "); Serial.print(gyro.gyro.z);     Serial.println("  \tdps");
-//  Serial.print("Temp: "); Serial.print(temp.temperature); Serial.println(" *C");
-
-  delay(10);
-}
-
-void calibrate(float x, float y) {
-   xRange[0] = min(xRange[0], x);
-   xRange[1] = max(xRange[1], x);
-   
-   yRange[0] = min(yRange[0], y);
-   yRange[1] = max(yRange[1], y);
-   
-   xNormal = (xRange[0] + xRange[1])/2;
-   yNormal = (yRange[0] + yRange[1])/2;
-   
-   Serial.print(xRange[0]); Serial.print(", "); Serial.println(xRange[1]);
-   Serial.print(yRange[0]); Serial.print(", "); Serial.println(yRange[1]);
- }
- 
-template<typename T>
-T clamp(T Value, T Min, T Max) {
-  return (Value < Min)? Min : (Value > Max)? Max : Value;
 }
